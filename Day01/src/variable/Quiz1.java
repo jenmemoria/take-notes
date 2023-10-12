80895ae0fc10cab9da9f00e89ea6e3fb2f12061f
jenmemoria <jenqosue@gmail.com>
2023-10-12 15:02:29 +0900

    aca

--- Day01/src/variable/Quiz1.java
+++ Day01/src/variable/Quiz1.java
@@ -3,10 +3,13 @@ package variable;
 public class Quiz1 {
 	public static void main(String[] args) {
 		
+		// 위 두 변수에 저장된 값을 활요하여 키와 체중을 소수점 아래자리 없이 정수형태로 출력
+		// (자료형 변환을 활용하여 새로운 변수에 저장하고 값을 출력)
+		
 		double height = 172.8;
 		double weight = 67.4;
 		
-		System.out.println((int)height);
+		System.out.println((int)172.8);
 		System.out.println((int)67.4);
 		
 		int height2 = (int)height;
@@ -15,7 +18,8 @@ public class Quiz1 {
 		System.out.println(height2);
 		System.out.println(weight2);
 		
-		// 위 두 변수에 저장된 값을 활요하여 키와 체중을 소수점 아래자리 없이 정수형태로 출력
-		// (자료형 변환을 활용하여 새로운 변수에 저장하고 값을 출력)
+		// 선생님의 의도 : 실수는 소수점까지 표현되지만, 정수는 소수점을 표현하지 않는다.
+		// 실수 값을 정수형 변수에 옮겨담으면 소수점은 잘린다. (의도된 데이터 손실)
+		// Doctor tmp = (Doctor)client04;
 	}
 }
---
+++ Day01/src/variable/Quiz2.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Quiz2 {
+	public static void main(String[] args) {
+		// 자신의 이름을 문자열 변수에 저장하세요.
+		String name = "왕제은";
+		// 자신의 나이를 정수형 변수에 저장하세요.
+		int age = 24;
+		Integer age2 = 24;
+		// 새로운 실수형 변수에 0을 대입하세요.
+		double n1 = 0;
+		Double n2 = (double)0;
+		// 방금 만든 실수형 변수에 3.14을 대입하세요.
+		n1 = 3.14;
+		n2 = 3.14;
+		// 지금까지 만든 3개 변수에 담긴 값을 각각 한 줄씩 화면에 출력하세요.
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(n1);
+		
+		System.out.println(name);
+		System.out.println(age2);
+		System.out.println(n2);
+	}
+}
